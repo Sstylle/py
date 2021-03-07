5daf9287b8813f3caa8a758c671b04b4ff813d53
Sstylle <786697810@qq.com>
2021-03-08 00:35:26 +0800

    send email1111

--- common/baidu_page.py
+++ common/baidu_page.py
@@ -1,17 +1,22 @@
 # coding:utf-8
 
-# from selenium import webdriver
+from selenium import webdriver
+from common.base import BasePage
 
-class BaiduPage():
+
+class BaiduPage(BasePage):
 
     # def __init__(self, driver):
     #     self.driver = webdriver.Chrome()
+    url = 'https://www.baidu.com'
 
-    def __init__(self, driver):
-        self.driver = driver
+    # def __init__(self, driver):
+    #     self.driver = driver
 
     def search_input(self, serch_key):
-        self.driver.find_element_by_id('kw').send_keys(serch_key)
+        # self.driver.find_element_by_id('kw').send_keys(serch_key)
+        self.by_id('kw').send_keys(serch_key)
 
     def search_button(self):
-        self.driver.find_element_by_id('su').click()
+        # self.driver.find_element_by_id('su').click()
+        self.by_id('su').click()
